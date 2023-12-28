ac386fb6ce1a95f7eac5fe6460ba447342570c68
Steven Pickles <stevenpickles@gmail.com>
2023-12-28 11:47:49 -0500

    Add app_config to set all command line parameters to usable values

---
+++ src/app_config.rs
@@ -0,0 +1,81 @@
+use clap::ArgMatches;
+use std::error;
+
+#[derive(Debug)]
+pub enum Protocol {
+    Tcp,
+    Udp,
+}
+
+#[derive(Debug)]
+pub enum Mode {
+    Client,
+    Server,
+}
+
+#[derive(Debug)]
+pub struct AppConfig {
+    pub mode: Mode,
+    pub remote_url: String,
+    pub remote_port: u16,
+    pub local_port: u16,
+    pub data_payload: String,
+    pub count: u32,
+    pub protocol: Protocol,
+    pub timeout_in_seconds: f64,
+}
+
+impl AppConfig {
+    pub fn new(clargs: &ArgMatches) -> Result<Self, Box<dyn error::Error>> {
+        let is_client_mode = clargs.contains_id("remote_url");
+        let mode = if is_client_mode {
+            Mode::Client
+        } else {
+            Mode::Server
+        };
+
+        let Some(remote_url) = clargs.get_one::<String>("remote_url") else {
+            return Err("remote_url is invalid".into());
+        };
+
+        let Some(remote_port) = clargs.get_one::<u16>("remote_port") else {
+            return Err("remote_port is invalid".into());
+        };
+
+        let Some(local_port) = clargs.get_one::<u16>("local_port") else {
+            return Err("local_port is invalid".into());
+        };
+
+        let Some(data_payload) = clargs.get_one::<String>("data_payload") else {
+            return Err("data_payload is invalid".into());
+        };
+
+        let Some(count) = clargs.get_one::<u32>("count") else {
+            return Err("count is invalid".into());
+        };
+
+        let Some(timeout_in_seconds) = clargs.get_one::<f64>("timeout_in_seconds") else {
+            return Err("timeout_in_seconds is invalid".into());
+        };
+
+        let protocol = match clargs.get_one::<String>("protocol") {
+            Some(value) => match value.as_str() {
+                "tcp" => Protocol::Tcp,
+                "udp" => Protocol::Udp,
+                _ => return Err("protocol is invalid".into()),
+            },
+            None => return Err("protocol is invalid".into()),
+        };
+
+        Ok(Self {
+            mode,
+            remote_url: remote_url.to_string(),
+            remote_port: *remote_port,
+            local_port: *local_port,
+            data_payload: data_payload.to_string(),
+            count: *count,
+            protocol,
+            timeout_in_seconds: *timeout_in_seconds,
+        })
+    }
+}
